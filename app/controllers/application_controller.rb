df585f36032d92a778e509fa0656832d0d1321d6
James Rogers <jarogers095@gmail.com>
2020-04-22 09:18:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,6 +48,7 @@ class ApplicationController < Sinatra::Base
   
   delete "articles/:id" do
     @article = Article.find(params[:id])
+    puts @article
     @article.destroy
     
     redirect to "/articles"
