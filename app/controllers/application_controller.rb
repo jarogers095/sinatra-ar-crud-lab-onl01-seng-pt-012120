30f1f1b4ee98f92817f6d162805046239282abe5
James Rogers <jarogers095@gmail.com>
2020-04-22 09:01:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
     @article = Article.find(params[:id])
     @article.title = params["title"]
     @article.content = params["content"]
-    @article.save
+    @article.update
     
     redirect to "/articles/#{@article.id}"
   end
